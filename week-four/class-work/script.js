51d7ebfe25d1d71ecf0ae023baa688d007c5e875
Yalda Samim <yalda.samim@powercoders.org>
2021-04-29 14:31:52 +0000

    change

---
+++ week-four/class-work/script.js
@@ -0,0 +1,65 @@
+  /*function greeting(myName) {
+  console.log(arguments);
+  return "hello " + "mary" + ".";
+}
+let greetings = greeting("");
+console.log(greetings);
+*/
+
+/* userName = function(){
+   return prompt("Whats your name?");
+  
+ };
+
+console.log(userName());
+*/
+
+/*
+let personName =prompt("please enter your name");
+if(personName == ""){
+  prompt("please enter the name");
+}
+function countChar(name){
+  return name.length;
+}
+
+console.log(countChar(personName));
+*/
+/*
+ let author = prompt("Please enter the author name");
+ while(author == ""){
+  prompt("please enter a name");
+}
+
+function quote(name){
+  return  author ;
+}
+ console.log("welcome to the world" + " " + quote(author));
+ */
+
+/*
+let birthYear =Number(prompt("enter your birth year"));
+let futureYear =Number(prompt("enter the future year"));
+let futureAge = calculateAge
+
+
+ function calculateAge(bY,fY){ 
+ return  fY - bY;
+}
+console.log("You will be" +  calculateAge(birthYear,futureYear));
+console.log("You will be" +  calculateAge(1990,2000));
+console.log("You will be" +  calculateAge(1856,3000));
+console.log("You will be" +  calculateAge(1,45));
+*/
+
+/*
+let number1 = Number(prompt("please enter the first number"));
+let number2 = Number(prompt("please enter the second number"));
+let greaterNumber = greaterNum
+
+function greaterNum(number1,number2) {
+  return Math.max(number1,number2);
+}
+
+console.log("the greater number between" + number1 + "and" + number2 + "is" + greaterNum(number1,number2));
+*/
